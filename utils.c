5b23db65fab4a5076c281de7753e68fed1141258
trivopia <135800895+trivopia@users.noreply.github.com>
2025-06-01 14:03:16 +0700

    modified:   main modified:   main.c new file:   utils.c new file:   utils.h
    Created up utils.h and utils.c and linked them to main.c
    Theses are files for utility functions such as getChar, getString, getInt
    
    To do: create the utility functions

---
+++ utils.c
@@ -0,0 +1,4 @@
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
---
+++ utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
